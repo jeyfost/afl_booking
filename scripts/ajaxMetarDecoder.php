d3a140dc2b63ed4b646497f3ea3f41e389ea423a
jeyfost <foster_andrew@tut.by>
2016-04-14 16:38:02 +0300

    В дешифровщик добавлена расшифровка видимости по полосам

--- scripts/ajaxMetarDecoder.php
+++ scripts/ajaxMetarDecoder.php
@@ -2,7 +2,7 @@
 
 include ('connect.php');
 
-$_POST['metar'] = "KBDL 131351Z 03013G21KT 010V050 10SM FEW040 FEW250 08/M03 A3041";
+$_POST['metar'] = "LLBG 140830Z 10010KT 050V150 0050S5000W R04/P1500N R22/0200V1000D FG VV001 15/15 Q1012";
 
 $data = explode(' ', $_POST['metar']);
 
@@ -19,7 +19,7 @@ $day = substr($data[$counter], 0, 2);
 $hours = substr($data[$counter], 2, 2);
 $minutes = substr($data[$counter], 4, 2);
 
-$time = $hours." ";
+$time = $day."-е число этого месяца, ".$hours." ";
 
 if(substr($hours, 0, 1) != "1") {
     if(substr($hours, 1) == "1") {
@@ -48,6 +48,10 @@ if(substr($minutes, 0, 1) != "1") {
 $time  = $time.$word." ".$minutes." ".$m_word." по UTC";
 $counter++;
 //////////////////////////////////////////////////////
+
+if($data[$counter] == "AUTO") {
+    $counter++;
+}
 
 //анализатор ветра
 $windDirection = substr($data[$counter], 0, 3);
@@ -192,8 +196,345 @@ if($windSpeed > 0) {
 if($data[$counter] == "CAVOK") {
     $visibility = "свыше 10 км, нет облаков ниже 5000 футов или минимальной высоты сектора (в зависимости от того, какое из значений больше), нет никаких погодных явлений на аэродроме и в его окрестностях";
 } else {
+    if(strpos($data[$counter], "SM") === false) {
+        if($data[$counter] == 9999) {
+            $visibility = "свыше 10 км";
+            $counter++;
+        } else {
+            if(strlen($data[$counter]) == 5 or strlen($data[$counter]) == 6) {
+                $distance = (int)substr($data[$counter], 0, 4);
+                $direction = substr($data[$counter], 4);
 
+                switch($direction){
+                    case "N":
+                        $direction = "северном";
+                        break;
+                    case "NE":
+                        $direction = "северо-восточном";
+                        break;
+                    case "NW":
+                        $direction = "северо-западном";
+                        break;
+                    case "E":
+                        $direction = "восточном";
+                        break;
+                    case "S":
+                        $direction = "южном";
+                        break;
+                    case "SE":
+                        $direction = "юго-восточном";
+                        break;
+                    case "SW":
+                        $direction = "юго-западном";
+                        break;
+                    case "W":
+                        $direction = "западном";
+                        break;
+                    default:
+                        break;
+                }
+
+                $visibility = $distance." метров в ".$direction." напрвлении";
+                $counter++;
+            } else {
+                if(strlen($data[$counter]) > 6) {
+                    $distance1 = (int)substr($data[$counter], 0, 4);
+
+                    if(ctype_alpha(substr($data[$counter], 4, 2))) {
+                        $start = 6;
+                        $direction1 = substr($data[$counter], 4, 2);
+                    } elseif(ctype_alpha(substr($data[$counter], 4, 1))) {
+                        $start = 5;
+                        $direction1 = substr($data[$counter], 4, 1);
+                    }
+
+                    switch($direction1){
+                        case "N":
+                            $direction1 = "северном";
+                            break;
+                        case "NE":
+                            $direction1 = "северо-восточном";
+                            break;
+                        case "NW":
+                            $direction1 = "северо-западном";
+                            break;
+                        case "E":
+                            $direction1 = "восточном";
+                            break;
+                        case "S":
+                            $direction1 = "южном";
+                            break;
+                        case "SE":
+                            $direction1 = "юго-восточном";
+                            break;
+                        case "SW":
+                            $direction1 = "юго-западном";
+                            break;
+                        case "W":
+                            $direction1 = "западном";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    $distance2 = (int)substr($data[$counter], $start, 4);
+                    $direction2 = substr($data[$counter], $start + 4);
+
+                    switch($direction2){
+                        case "N":
+                            $direction2 = "северном";
+                            break;
+                        case "NE":
+                            $direction2 = "северо-восточном";
+                            break;
+                        case "NW":
+                            $direction2 = "северо-западном";
+                            break;
+                        case "E":
+                            $direction2 = "восточном";
+                            break;
+                        case "S":
+                            $direction2 = "южном";
+                            break;
+                        case "SE":
+                            $direction2 = "юго-восточном";
+                            break;
+                        case "SW":
+                            $direction2 = "юго-западном";
+                            break;
+                        case "W":
+                            $direction2 = "западном";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    $visibility = $distance1." метров в ".$direction1." напрвлении и ".$distance2." метров в ".$direction2." направлении";
+                    $counter++;
+                } else {
+                    $visibility = (int)$data[$counter]." метров";
+                    $counter++;
+                }
+            }
+        }
+    } else {
+        $pos = strpos($data[$counter], "SM");
+        $distance = substr($data[$counter], 0, $pos);
+
+        if(strlen($distance) == 1) {
+            if($distance == 1) {
+                $distanceMeasure = "миля";
+            } elseif($distance == 2 or $distance == 3 or $distance == 4) {
+                $distanceMeasure = "мили";
+            } else {
+                $distanceMeasure = "миль";
+            }
+        } else {
+            if(substr($distance, 1, 1) == "/") {
+                $distanceMeasure = "мили";
+            } else {
+                if (substr($distance, 0, 1) == "1") {
+                    $distanceMeasure = "миль";
+                } else {
+                    if (substr($distance, 1) == "2" or substr($distance, 1) == "3" or substr($distance, 1) == "4") {
+                        $distanceMeasure = "мили";
+                    } elseif (substr($distance, 1) == "1") {
+                        $distanceMeasure = "миля";
+                    } else {
+                        $distanceMeasure = "миль";
+                    }
+                }
+            }
+        }
+
+        $visibility = $distance." ".$distanceMeasure;
+        $counter++;
+    }
 }
+
+//анализатор видимости по полосе
+if(substr($data[$counter], 0, 1) == "R" and is_numeric(substr($data[$counter], 1, 2))) {
+    if(ctype_alpha(substr($data[$counter], 3, 1))) {
+        $runway = substr($data[$counter], 1, 3);
+        $start = 4;
+    } else {
+        $runway = substr($data[$counter], 1, 2);
+        $start = 3;
+    }
+
+    if(substr($data[$counter], $start + 1, 1) == "P" or substr($data[$counter], $start + 1, 1) == "M") {
+        switch(substr($data[$counter], $start + 1, 1)) {
+            case "P":
+                $visibilityIs = "более";
+                break;
+            case "M":
+                $visibilityIs = "менее";
+                break;
+            default:
+                break;
+        }
+
+        if(ctype_alpha(substr($data[$counter], $start + 6))) {
+            switch(substr($data[$counter], $start + 6)) {
+                case "D":
+                    $visibilityForecast = "Прогнозируется ухудшение видимости";
+                    break;
+                case "U":
+                    $visibilityForecast = "Прогнозируется улучшение видимости";
+                    break;
+                case "N":
+                    $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                    break;
+                default:
+                    break;
+            }
+
+            $visibility .= ". Видимость на полосе ".$runway." ".$visibilityIs." ".(int)substr($data[$counter], $start + 2, 4)." метров. ".$visibilityForecast;
+            $counter++;
+        } else {
+            $visibility .= ". Видимость на полосе ".$runway." ".$visibilityIs." ".(int)substr($data[$counter], $start + 2, 4)." метров.";
+            $counter++;
+        }
+    } else {
+        if(substr($data[$counter], $start + 5, 1) == "V") {
+            if(ctype_alpha(substr($data[$counter], $start + 10))) {
+                switch(substr($data[$counter], $start + 10)) {
+                    case "D":
+                        $visibilityForecast = "Прогнозируется ухудшение видимости";
+                        break;
+                    case "U":
+                        $visibilityForecast = "Прогнозируется улучшение видимости";
+                        break;
+                    case "N":
+                        $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                        break;
+                    default:
+                        break;
+                }
+
+                $visibility .= ". Видимость на полосе ".$runway." варьируется от ".(int)substr($data[$counter], $start + 1, 4)." до ".(int)substr($data[$counter], $start + 6, 4)." метров. ".$visibilityForecast;
+                $counter++;
+            } else {
+                $visibility .= ". Видимость на полосе ".$runway." варьируется от ".(int)substr($data[$counter], $start + 1, 4)." до ".(int)substr($data[$counter], $start + 6)." метров.";
+                $counter++;
+            }
+        } else {
+            if(ctype_alpha(substr($data[$counter], $start + 5))) {
+                switch(substr($data[$counter], $start + 5)) {
+                    case "D":
+                        $visibilityForecast = "Прогнозируется ухудшение видимости";
+                        break;
+                    case "U":
+                        $visibilityForecast = "Прогнозируется улучшение видимости";
+                        break;
+                    case "N":
+                        $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                        break;
+                    default:
+                        break;
+                }
+                $visibility .= ". Видимость на полосе ".$runway." составляет ".(int)substr($data[$counter], $start + 1, 4)." метров. ".$visibilityForecast;
+                $counter++;
+            } else {
+                $visibility .= ". Видимость на полосе ".$runway." составляет ".(int)substr($data[$counter], $start + 1)." метров";
+                $counter++;
+            }
+        }
+    }
+}
+//////////////////////////////////////////////////////
+
+//анализатор видимости по другой полосе, если она есть
+if(substr($data[$counter], 0, 1) == "R" and is_numeric(substr($data[$counter], 1, 2))) {
+    if(ctype_alpha(substr($data[$counter], 3, 1))) {
+        $runway = substr($data[$counter], 1, 3);
+        $start = 4;
+    } else {
+        $runway = substr($data[$counter], 1, 2);
+        $start = 3;
+    }
+
+    if(substr($data[$counter], $start + 1, 1) == "P" or substr($data[$counter], $start + 1, 1) == "M") {
+        switch(substr($data[$counter], $start + 1, 1)) {
+            case "P":
+                $visibilityIs = "более";
+                break;
+            case "M":
+                $visibilityIs = "менее";
+                break;
+            default:
+                break;
+        }
+
+        if(ctype_alpha(substr($data[$counter], $start + 6))) {
+            switch(substr($data[$counter], $start + 6)) {
+                case "D":
+                    $visibilityForecast = "Прогнозируется ухудшение видимости";
+                    break;
+                case "U":
+                    $visibilityForecast = "Прогнозируется улучшение видимости";
+                    break;
+                case "N":
+                    $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                    break;
+                default:
+                    break;
+            }
+
+            $visibility .= ". Видимость на полосе ".$runway." ".$visibilityIs." ".(int)substr($data[$counter], $start + 2, 4)." метров. ".$visibilityForecast;
+            $counter++;
+        } else {
+            $visibility .= ". Видимость на полосе ".$runway." ".$visibilityIs." ".(int)substr($data[$counter], $start + 2, 4)." метров.";
+            $counter++;
+        }
+    } else {
+        if(substr($data[$counter], $start + 5, 1) == "V") {
+            if(ctype_alpha(substr($data[$counter], $start + 10))) {
+                switch(substr($data[$counter], $start + 10)) {
+                    case "D":
+                        $visibilityForecast = "Прогнозируется ухудшение видимости";
+                        break;
+                    case "U":
+                        $visibilityForecast = "Прогнозируется улучшение видимости";
+                        break;
+                    case "N":
+                        $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                        break;
+                    default:
+                        break;
+                }
+
+                $visibility .= ". Видимость на полосе ".$runway." варьируется от ".(int)substr($data[$counter], $start + 1, 4)." до ".(int)substr($data[$counter], $start + 6, 4)." метров. ".$visibilityForecast;
+                $counter++;
+            } else {
+                $visibility .= ". Видимость на полосе ".$runway." варьируется от ".(int)substr($data[$counter], $start + 1, 4)." до ".(int)substr($data[$counter], $start + 6)." метров.";
+                $counter++;
+            }
+        } else {
+            if(ctype_alpha(substr($data[$counter], $start + 5))) {
+                switch(substr($data[$counter], $start + 5)) {
+                    case "D":
+                        $visibilityForecast = "Прогнозируется ухудшение видимости";
+                        break;
+                    case "U":
+                        $visibilityForecast = "Прогнозируется улучшение видимости";
+                        break;
+                    case "N":
+                        $visibilityForecast = "Значительных изменений в видимости не прогнозируется";
+                        break;
+                    default:
+                        break;
+                }
+                $visibility .= ". Видимость на полосе ".$runway." составляет ".(int)substr($data[$counter], $start + 1, 4)." метров. ".$visibilityForecast;
+                $counter++;
+            } else {
+                $visibility .= ". Видимость на полосе ".$runway." составляет ".(int)substr($data[$counter], $start + 1)." метров";
+                $counter++;
+            }
+        }
+    }
+}
+//////////////////////////////////////////////////////
 
 
 echo "Погода в аэропорту <img src='img/flags/".$airport['iso_code'].".png' title='".$airport['country']."' /> <a href='http://va-aeroflot.su/airport/".$data[0]."' style='margin-left: 0;'>".$airport['name']." (".$data[0].")"."</a> по состоянию на ".$time.":<br /><br /><b>Ветер у земли</b>: ".$windTotal."<br /><br /><b>Видимость</b>: ".$visibility;
